f576c340532c012c54112b36e78f02f7ff68e4f1
key <ksyuocs@gmail.com>
2025-05-27 11:38:41 +0900

    な行と全体の修正

--- sample.java
+++ sample.java
@@ -1,15 +1,15 @@
 public class sample {
     public static void main(String[] args) {
-        String word_na= "ありがたやありがたや";
+        String word_na= "なあなあ、さやって知ってる？";
         String word_ni = "２千年前の記憶が蘇る、、";
-        String word_nu = "濡れたあの子に傘をさす";
-        String word_ne = "ねぇねぇ、一緒にねるねるねるねのうえでおねんねしない？";
+        String word_nu = "抜き足差し足忍び足";
+        String word_ne = "駅で急に待ち合わせ。慌てて準備して";
         String word_no = "のんびりと過ごして楽しかった";
 
-        System.out.println("あ：" + word_a);
-        System.out.println("い：" + word_i);
+        System.out.println("な：" + word_na);
+        System.out.println("に：" + word_ni);
         System.out.println("ぬ：" + word_nu);
-        System.out.println("え：" + word_e);
-        System.out.println("お：" + word_o);
+        System.out.println("ね：" + word_ne);
+        System.out.println("の：" + word_no);
     }
 }
