25e0b373b5c3aa5e19070fcba0d71d5456ed0ad1
urata234017 <johoyouakunt@gmail.com>
2025-05-27 11:43:11 +0900

    ね
    「」のついか

--- sample.java
+++ sample.java
@@ -3,7 +3,7 @@ public class sample {
         String word_na= "ありがたやありがたや";
         String word_ni = "２千年前の記憶が蘇る、、";
         String word_nu = "濡れたあの子に傘をさす";
-        String word_ne = "ねぇねぇ、一緒にねるねるねるねのうえでおねんねしない？";
+        String word_ne = "「ねぇねぇ、一緒にねるねるねるねのうえでおねんねしない？」";
         String word_no = "のんびりと過ごして楽しかった";
 
         System.out.println("あ：" + word_na);
