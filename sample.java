05169a0ac101ee9861be8a0963d14d673a144520
Taise_2525 <95949607+zebra2525Tai@users.noreply.github.com>
2025-05-27 11:21:53 +0900

    にの変更
    輪廻転生か！？！？！

--- sample.java
+++ sample.java
@@ -1,7 +1,7 @@
 public class sample {
     public static void main(String[] args) {
         String word_a = "朝から驚きの連続だ";
-        String word_ni = "にっこにこな笑顔で";
+        String word_ni = "２千年前の記憶が蘇る、、";
         String word_u = "嬉しい知らせが飛び込んできた";
         String word_e = "駅で急に待ち合わせ。慌てて準備して";
         String word_o = "温泉旅行に出発だ！";
