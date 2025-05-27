485c6239ab3ae328545676088c53d18bb431274e
takoyakigod17 <tkjohor3sb09@gmail.com>
2025-05-27 11:23:22 +0900

    の行の変更

--- sample.java
+++ sample.java
@@ -4,7 +4,7 @@ public class sample {
         String word_i = "いつもの朝とは様子が違う";
         String word_u = "嬉しい知らせが飛び込んできた";
         String word_e = "駅で急に待ち合わせ。慌てて準備して";
-        String word_o = "のび太君の大冒険";
+        String word_o = "のんびりと過ごして楽しかった";
 
         System.out.println("あ：" + word_a);
         System.out.println("い：" + word_i);
